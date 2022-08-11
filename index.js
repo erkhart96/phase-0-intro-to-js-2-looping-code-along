cf7a6b075b91f5fe039fdb6e419c1c21e4c3d5c3
Ben <berkhart@rocketmail.com>
2022-08-11 12:19:33 -0500

    commit tree 3dcab8fc87f87ed9b7feeb2c70457177b4dd3664

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solutions in this file
+function writeCards (people, e) {
+    const messages = [];
+    for (let i = 0; i < people.length; i++) {
+        const message = `Thank you, ${people[i]}, for the wonderful ${e} gift!`
+        messages.push(message);
+    }
+    return messages;
+}
+
+writeCards(['Guadalupe', 'Ollie', 'Aki'], 'surprise');
+
+function countDown(n) {
+    console.log(n);
+    let countdown = n;
+    while (countdown > 0) {
+        countdown--;
+        console.log(countdown);
+    }
+}
+
+countdown(10);
